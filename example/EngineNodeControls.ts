08d375b7e4517cbd423502c2a7d12793c552a16a
John Ellmore <jnellmore@gmail.com>
2020-09-05 16:47:09 -0500

    Update chain, add controls

---
+++ example/EngineNodeControls.ts
@@ -0,0 +1,115 @@
+import ChainNode from "src/ChainNode";
+import { Engine } from "src/engine";
+import { html } from "htm/preact";
+import { Component } from "preact";
+import { useState, useCallback } from "preact/hooks";
+import { DecibelParameter, HertzParameter } from "src/Parameter";
+
+function DecibelControl(props) {
+  const param = props.param as DecibelParameter;
+  const [decibelValue, setDecibelValue] = useState(param.value);
+
+  const handleChange = useCallback(
+    (event) => {
+      const input = event.target as HTMLInputElement;
+      const db = parseFloat(input.value);
+      param.value = db;
+      setDecibelValue(db);
+    },
+    [decibelValue]
+  );
+
+  return html`<div>
+    <label>
+      ${param.label}
+      <br />
+      <input
+        type="range"
+        min="-100"
+        max="20"
+        step="0.1"
+        value=${decibelValue}
+        onInput=${handleChange} />
+      ${decibelValue.toFixed(2)} dB
+    </label>
+  </div>`;
+}
+
+function HertzControl(props) {
+  const [min, max] = [20, 20000];
+  const intervalToFreq = (pos) =>
+    10 **
+    ((Math.log10(max) - Math.log10(min)) * (pos / 1000) + Math.log10(min));
+  const freqToInterval = (freq) =>
+    ((Math.log10(freq) - Math.log10(min)) /
+      (Math.log10(max) - Math.log10(min))) *
+    1000;
+
+  const param = props.param as DecibelParameter;
+  const [intervalValue, setIntervalValue] = useState(
+    freqToInterval(param.value)
+  );
+
+  const handleChange = useCallback(
+    (event) => {
+      const input = event.target as HTMLInputElement;
+      const intervalVal = parseFloat(input.value);
+      console.log(intervalVal);
+      setIntervalValue(intervalVal);
+      const hz = intervalToFreq(intervalVal);
+      param.value = hz;
+    },
+    [intervalValue]
+  );
+
+  return html`<div>
+    <label>
+      ${param.label}
+      <br />
+      <input
+        type="range"
+        min="0"
+        max="1000"
+        step="1"
+        value=${intervalValue}
+        onInput=${handleChange} />
+      ${intervalToFreq(intervalValue).toFixed(1)} Hz
+    </label>
+  </div>`;
+}
+
+class NodeControls extends Component {
+  render(props, state) {
+    const node: ChainNode = props.node;
+    return html`
+      <h2>${node.constructor.name}</h2>
+      ${node.params.map((param) => {
+        if (param.constructor === DecibelParameter) {
+          return html`<${DecibelControl} param=${param} />`;
+        } else if (param.constructor === HertzParameter) {
+          return html`<${HertzControl} param=${param} />`;
+        } else {
+          return html`<em>No param control available</em>`;
+        }
+      })}
+    `;
+  }
+}
+
+export default class EngineNodeControls extends Component {
+  render(props, state) {
+    const engine: Engine = props.engine;
+    return html`
+      <h1>Chain Nodes:</h1>
+      <ul>
+        ${engine.nodes.map((node) => {
+          return html`
+          <li>
+          <${NodeControls} node=${node} />
+          </li>
+        `;
+        })}
+      </ul>
+    `;
+  }
+}
--- example/chain.ts
+++ example/chain.ts
@@ -1,21 +1,30 @@
 import engine from "src/engine";
 import { dB, Hz, ms } from "src/units";
+import EngineNodeControls from "./EngineNodeControls";
+import { render } from "preact";
+import { html } from "htm/preact";
 
 const eng = engine((tools) => {
-  const { sine, gain, mix, noiseGate } = tools;
+  const { pipe, sine, gain, mix, noiseGate } = tools;
 
-  const lowSignal = sine(Hz(300)).pipe(
+  const lowSignal = pipe(
+    sine(Hz(300)),
     noiseGate(dB(-30), ms(10), ms(100), ms(40)), // thres, attack, hold, release
     // highPassFilter(300), // HPF filter at 300Hz
     // compressor(-20, 0.25), // compress anything above -20dB at a 4:1 ratio
-    gain(dB(-3)) // drop volume by -15dB
+    gain(dB(-3)), // drop volume by -15dB
   );
 
-  const highSignal = sine(Hz(800)).pipe(
+  const highSignal = pipe(
+    sine(Hz(800)),
     // highPassFilter(300), // HPF filter at 300Hz
     // compressor(-20, 0.25), // compress anything above -20dB at a 4:1 ratio
-    gain(dB(-15)) // drop volume by -15dB
+    gain(dB(-5)), // drop volume by -15dB
   );
 
-  return mix(lowSignal, highSignal);
+  return pipe(mix(lowSignal, highSignal), gain(dB(-20)));
 });
+
+// mount the engine to the root of the page
+const app = html`<${EngineNodeControls} engine=${eng} />`;
+render(app, document.body);
